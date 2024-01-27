17acae16282b2766959043e5d718b043234cd696
AlexKul713 <uli891993@yandex.ru>
2024-01-27 15:14:42 +0300

    work db

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -3,9 +3,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
-DEBUG = True
-ALLOWED_HOSTS = []
+SECRET_KEY = os.getenv('SECRET_KEY', '123')
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
+
+DEBUG = os.getenv('DEBUG', default=True)
 
 # Application definition
 
